138c62df1f3d28a31c43278829263a2b778e3765
Liana Buss <lianabuss@gmail.com>
2017-04-06 22:07:50 -0300

    #Dev: Finished the posts new and route to the index. Created the posts_show and configurated the rote to show the posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,6 +9,19 @@ class PostIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+		return this.props.posts.map((post) => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					<Link to={"posts/" + post.id}>
+						<span className="pull-xs-right">{post.categories}</span>
+						<strong>{post.title}</strong>
+					</Link>
+				</li>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<div>
@@ -17,9 +30,17 @@ class PostIndex extends Component {
 				  Add a post
 				</Link>
 			  </div>
+			  <h3>Posts</h3>
+			  <ul className="list-group">
+			  	{this.renderPosts()}
+			  </ul>
 			</div>
 		);
 	}
 }
 
-export default connect(null, { fetchPosts })(PostIndex);
+function mapStateToProps(state) {
+	return { posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
